e33d127863f5c0ad3f5434f65303c57fd133ec73
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-09 17:19:09 +0200

    show data in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,42 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<body class="bg-dark">
+    <header>
+        <h1 class="text-warning text-center mt-5">LARAVEL-MIGRATION-SEEDER</h1>
+    </header>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <main class="container mt-5">
+        <div class="row">
+            @forelse ($trains as $train)
+                <div class="col-4">
+                    <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+                        <div class="card-header text-capitalize fw-semibold">{{ $train->departure_station }} -
+                            {{ $train->arrival_station }}</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-capitalize">{{ $train->agency }}</h5>
+                            <div class="card-text text-capitalize"><span class="fw-semibold">partenza:</span> {{ $train->departure_time }}</div>
+                            <div class="card-text text-capitalize"><span class="fw-semibold">arrivo:</span> {{ $train->arrival_time }}</div>
+                            <div class="card-text text-capitalize"><span class="fw-semibold">codice treno:</span> {{ $train->train_code }}</div>
+                            @if (!$train->in_time)
+                                <div class="card-text text-capitalize fw-semibold">in ritardo</div>
+                            @endif
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            @empty
+                <div>non pervenuti</div>
+            @endforelse
         </div>
-    </body>
+    </main>
+</body>
+
 </html>
